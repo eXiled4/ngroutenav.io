f94d24c3eba79f99ab47d3606cbca1a8552d9145
braum <elite21ret@gmail.com>
2022-07-12 15:04:44 +1000

    added another example with different pathname:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 
 const routes: Routes = [
-  { path: '', component: FirstComponent, pathMatch: 'full' }
+  { path: '', component: FirstComponent, pathMatch: 'full' },
+  { path: 'rabble', component: FirstComponent }
 ];
 
 @NgModule({
@@ -14,7 +15,7 @@ export class AppRoutingModule { }
 
 // To configure routing, search for this file app-routig.module.ts
 // in line 4 - the const routes: Routes = [];
-// Syntax as follows;
+// Syntax as follows; - this syntax is a configuration object - defines one path in your application
 // { path: '', component: FirstComponent }
 // path - extension to URL, inside single quotes is the location and extension ofter default URL eg localhost4200
 // next, state the component to be viewed
