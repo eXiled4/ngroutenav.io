4fae85c60119d8514da2616181d36efa8f5c7938
braum <elite21ret@gmail.com>
2022-07-12 15:01:29 +1000

    syntax for pathing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
@@ -11,3 +14,8 @@ export class AppRoutingModule { }
 
 // To configure routing, search for this file app-routig.module.ts
 // in line 4 - the const routes: Routes = [];
+// Syntax as follows;
+// { path: '', component: FirstComponent }
+// path - extension to URL, inside single quotes is the location and extension ofter default URL eg localhost4200
+// next, state the component to be viewed
+// pathMatch- requires user to enter the exact path to prevent error message
